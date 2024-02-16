ded2255f8f931dcdc2446d21d6375cc9e35ffce1
gabi <sandrogabidauri26@gmail.com>
2024-02-16 10:54:26 +0400

    still working

--- app/src/main/java/com/example/hw22/presentation/screen/activity/MainActivity.kt
+++ app/src/main/java/com/example/hw22/presentation/screen/activity/MainActivity.kt
@@ -1,14 +1,42 @@
 package com.example.hw22.presentation.screen.activity
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.os.Build
 import android.os.Bundle
-import com.example.hw22.R
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import com.example.hw22.databinding.ActivityMainBinding
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.messaging.FirebaseMessaging
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val requestPermission =
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) {}
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        requestPermission()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        readPushToken()
     }
+
+    private fun requestPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            requestPermission.launch(arrayOf(Manifest.permission.POST_NOTIFICATIONS))
+        }
+    }
+
+    private fun readPushToken() {
+        FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
+            if (!task.isSuccessful) {
+                return@OnCompleteListener
+            }
+        })
+    }
+
 }
